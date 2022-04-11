f5a324652649e0b5020510e15b834fd7e3924e65
Danilo Ewerton <danilo.ewe@gmail.com>
2022-04-11 09:11:15 -0300

    Commit inicial

---
+++ src/test/java/com/daniloewerton/minhasfinancas/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniloewerton.minhasfinancas.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
